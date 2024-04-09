b5b4ab2c153495aa493b56b922890c34181b9938
Andrii <andrii.chemer@workai.com>
2024-04-09 14:24:14 +0200

    refactor

--- lib/core/di/module/storage_module.dart
+++ lib/core/di/module/storage_module.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:injectable/injectable.dart';
 import 'package:iteo_libraries_example/data/app_theme/app_theme_type_storage_impl.dart';
 import 'package:iteo_libraries_example/data/database/drift_local_database.db.dart';
-import 'package:iteo_libraries_example/data/database/provider/native_database_provider.dart' as native;
-import 'package:iteo_libraries_example/data/database/provider/web_wasm_database_provider.dart' as web;
+import 'package:iteo_libraries_example/data/database/provider/shared.dart';
 import 'package:iteo_libraries_example/domain/app_theme/app_theme_type_storage.dart';
 import 'package:iteo_libraries_example/domain/security/security_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -25,7 +23,5 @@ abstract class StorageModule {
   @preResolve
   Future<DriftLocalDatabase> driftDatabase(
     SecurityRepository securityRepository,
-  ) => kIsWeb
-      ? web.getDriftDatabase(securityRepository)
-      : native.getDriftDatabase(securityRepository);
+  ) => getDriftDatabase(securityRepository);
 }
